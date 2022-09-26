04a6d1fbde934911c0f3af66d1f9fa5ba20e98f6
William Convertino <williamconvertino@gmail.com>
2022-09-26 15:16:11 +0200

    Very messy atm, but working on fixing the game management

---
+++ Assets/Scripts/Challenge/DevTools/TimedChallengeDevScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class TimedChallengeDevScript : ChallengeDevScript
+{
+    public float timer;
+
+    protected override void Start()
+    {
+        TimedChallenge challengeScript = gameObject.GetComponent<TimedChallenge>();
+        challengeScript.Init(players, timer);
+    }
+}
