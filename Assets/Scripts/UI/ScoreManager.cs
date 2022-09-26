35d50ba20119baf5595cd84f397147f77668afe8
Aarushi Venkatakrishnan <aarushi.venkatakrishnan@duke.edu>
2022-09-26 15:39:46 +0200

    created sample scene counting jumps

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -10,17 +10,24 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
 
     public TMP_Text scoreText;
+    public TMP_Text playerNum;
+    public string player;
 
     int score = 0;
 
     void Start()
     {
-        scoreText.text = score.ToString() + " POINTS";
+        scoreText.text = score.ToString() + " JUMPS";
+        playerNum.text = "PLAYER " + player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            score++;
+            scoreText.text = score.ToString() + " JUMPS";
+        }
     }
 }
