c4db68272da91e890566d370d6d0faf1b5713c57
William Convertino <williamconvertino@gmail.com>
2022-09-29 13:01:15 +0200

    Added crown minigame

---
+++ Assets/Scripts/Challenge/Challenges/Crown/CrownChallenge.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CrownChallenge : TimedChallenge
+{
+    #region Initialization
+    
+    private List<CrownPlayerScript> _playerScripts;
+    public override void Init(GameObject[] players)
+    {
+        base.Init(players);
+        AddScriptToPlayers<CrownPlayerScript>();
+        _playerScripts = GetPlayerScripts<CrownPlayerScript>();
+        CrownPlayerScript initialCrown = _playerScripts[Random.Range(0,_playerScripts.Count)];
+        initialCrown.Crown();
+        Debug.Log("Starting Crown");
+    }
+    #endregion
+
+    #region On Respawn
+
+    public override void OnPlayerRespawn(GameObject player)
+    {
+        CrownPlayerScript playerScript = player.GetComponent<CrownPlayerScript>();
+        if (!playerScript.Crowned || _playerScripts.Count < 2)
+        {
+            return;
+        }
+
+        CrownPlayerScript newCrowned = playerScript;
+        while (newCrowned == playerScript)
+        {
+            newCrowned = _playerScripts[Random.Range(0, _playerScripts.Count)];
+        }
+        playerScript.UnCrown();
+        newCrowned.Crown();
+    }
+
+    #endregion
+    
+    #region Victory Conditions
+
+    public override GameObject[] GetWinners()
+    {
+        List<GameObject> winners = new List<GameObject>();
+        foreach (CrownPlayerScript playerScript in _playerScripts)
+        {
+            if (playerScript.Crowned)
+            {
+                winners.Add(playerScript.Player);
+            }
+        }
+        return winners.ToArray();
+    }
+    public override GameObject[] GetLosers()
+    {
+        List<GameObject> losers = new List<GameObject>();
+        foreach (CrownPlayerScript playerScript in _playerScripts)
+        {
+            if (!playerScript.Crowned)
+            {
+                losers.Add(playerScript.Player);
+            }
+        }
+        return losers.ToArray();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Challenge/Challenges/Crown/CrownPlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+public class CrownPlayerScript : ChallengePlayerScript
+{
+    public bool Crowned { private set; get; }  = false;
+    private float _totalInvincibilityTime = 0.5f;
+    private float _invincibilityTimer = 0;
+
+    private SpriteRenderer _spriteRenderer;
+    private Color _originalColor;
+    public override void Init()
+    {
+        base.Init();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _originalColor = _spriteRenderer.color;
+    }
+    private void Update()
+    {
+        if (_invincibilityTimer > 0)
+        {
+            _invincibilityTimer -= Time.deltaTime;    
+        }
+    }
+
+    public override void OnPlayerCollision(GameObject other)
+    {
+        if (Crowned)
+        {
+            bool crownSuccessful = other.GetComponent<CrownPlayerScript>().Crown();
+            if (crownSuccessful)
+            {
+                UnCrown();
+            }
+        }
+    }
+
+    public void UnCrown()
+    {
+        Crowned = false;
+        _invincibilityTimer = _totalInvincibilityTime;
+        _spriteRenderer.color = _originalColor;
+    }
+
+    public bool Crown()
+    {
+        if (_invincibilityTimer > 0)
+        {
+            return false;
+        }
+        _spriteRenderer.color = Color.yellow;
+        return Crowned = true;
+    }
+
+    public override void Cleanup()
+    {
+        UnCrown();
+    }
+}
