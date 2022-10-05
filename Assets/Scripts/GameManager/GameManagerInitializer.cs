4de00487b44131d9e70c5b58e5c9ae6efae720c8
Alex Zhuang <azhuang639@gmail.com>
2022-10-05 19:04:07 +0200

    main menu now successfully loads new game scene

--- Assets/Scripts/GameManager/GameManagerInitializer.cs
+++ Assets/Scripts/GameManager/GameManagerInitializer.cs
@@ -7,19 +7,14 @@ class GameManagerInitializer : MonoBehaviour
 {
     private void Start()
     {
-        foreach (PlayerData player in GlobalSettingsSingleton.Instance.PlayerData)
-        {
-            player.playerInputPrefab = Instantiate(player.playerInputPrefab);
-        }
-
         GameManager.GameSettings gameSettings = new GameManager.GameSettings()
         {
             PlayerData = GlobalSettingsSingleton.Instance.PlayerData,
             ChallengePrefabs = GlobalSettingsSingleton.Instance.ChallengePrefabs,
             GameTime = GlobalSettingsSingleton.Instance.GameTime,
             TimeBetweenRounds = GlobalSettingsSingleton.Instance.TimeBetweenRounds,
-            StagePrefab = Instantiate(GlobalSettingsSingleton.Instance.StagePrefab),
-            ScoringSystemPrefab = Instantiate(GlobalSettingsSingleton.Instance.ScoringSystemPrefab)
+            StagePrefab = GlobalSettingsSingleton.Instance.StagePrefab,
+            ScoringSystemPrefab = GlobalSettingsSingleton.Instance.ScoringSystemPrefab
         };
         Instantiate(Resources.Load<GameManager>("Prefabs/GameManager/GameManager"), transform).Init(gameSettings);
     }
--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -25,31 +25,31 @@ public class MainMenuManager : MonoBehaviour
     {
         PlayerData testPlayerData = new PlayerData();
         testPlayerData.playerName = "A";
-        testPlayerData.playerID = 1;
+        testPlayerData.playerID = 0;
         testPlayerData.playerInputPrefab = Resources.Load<GameObject>("Prefabs/Player/Input/CoopLeftPlayerInput");
         testPlayerData.headSprite = null;
         testPlayerData.spriteColor = Color.red;
 
         PlayerData testPlayer2Data = new PlayerData();
         testPlayer2Data.playerName = "B";
-        testPlayer2Data.playerID = 2;
+        testPlayer2Data.playerID = 0;
         testPlayer2Data.playerInputPrefab = Resources.Load<GameObject>("Prefabs/Player/Input/CoopRightPlayerInput");
         testPlayer2Data.headSprite = null;
         testPlayer2Data.spriteColor = Color.blue;
 
         PlayerData testPlayer3Data = new PlayerData();
-        testPlayer2Data.playerName = "C";
-        testPlayer2Data.playerID = 3;
-        testPlayer2Data.playerInputPrefab = Resources.Load<GameObject>("Prefabs/Player/Input/CoopMiddlePlayerInput");
-        testPlayer2Data.headSprite = null;
-        testPlayer2Data.spriteColor = Color.green;
+        testPlayer3Data.playerName = "C";
+        testPlayer3Data.playerID = 0;
+        testPlayer3Data.playerInputPrefab = Resources.Load<GameObject>("Prefabs/Player/Input/CoopMiddlePlayerInput");
+        testPlayer3Data.headSprite = null;
+        testPlayer3Data.spriteColor = Color.green;
 
         GameObject testChallenge = Resources.Load<GameObject>("Prefabs/Challenge/Tag");
 
         GlobalSettingsSingleton.Instance.ScoringSystemPrefab = Resources.Load<GameObject>("Prefabs/ScoringSystem/PointScoringSystem");
         GlobalSettingsSingleton.Instance.StagePrefab = Resources.Load<GameObject>("Prefabs/Stages/Battlefield");
         GlobalSettingsSingleton.Instance.ChallengePrefabs = new List<GameObject> { testChallenge };
-        GlobalSettingsSingleton.Instance.PlayerData = new List<PlayerData> { testPlayerData, testPlayer2Data };
+        GlobalSettingsSingleton.Instance.PlayerData = new List<PlayerData> { testPlayerData, testPlayer2Data, testPlayer3Data };
         GlobalSettingsSingleton.Instance.GameTime = 60;
         GlobalSettingsSingleton.Instance.TimeBetweenRounds = 3;
 
