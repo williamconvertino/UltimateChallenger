0e3ac3c85a1ee2c416152e6a7cb524ea3a998737
Alex Zhuang <azhuang639@gmail.com>
2022-10-06 20:31:01 +0200

    got click object working

--- Assets/Scripts/Testing/ClickedObject.cs
+++ Assets/Scripts/Testing/ClickedObject.cs
@@ -4,28 +4,20 @@ using UnityEngine;
 
 public class ClickedObject : MonoBehaviour
 {
-    public Camera cameraa;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("huh!!!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        { // if left button pressed...
-            Ray ray = cameraa.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            print("NOT IT WORKED!");
-            if (Physics.Raycast(ray, out hit))
-            {
-                print("IT WORKED!");
-                // the object identified by hit.transform was clicked
-                // do whatever you want
-            }
-        }
+
+    }
+
+    void OnMouseDown()
+    {
+        print("YOOOOOO!!!");
     }
 }
